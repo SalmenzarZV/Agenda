70241b49794fe21dd36bb13a07e5a431a50b5af7
SergioAlmenzarIESZaidinVergeles20-21 <salmenzar1510@ieszaidinvergeles.org>
2021-12-16 21:57:57 +0100

    no sale :(

--- app/src/main/java/com/ad/saj/myapplication/MainActivity.java
+++ app/src/main/java/com/ad/saj/myapplication/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -30,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     private final int CONTACTS_PERMISSION = 1;
     private final String TAG = "sdfbdsi";
 
-    private Button btSearch; // = findViewById(); eso no se hace aqui aun no existe el layout
     private TextView tvResult;
     private EditText etPhone;
 
@@ -38,16 +40,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v(TAG, "onCreate");//verbose
+        Log.v(TAG, "onCreate");
         initialize();
     }
 
-    @Override
-
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
 
     @Override
     protected void onDestroy() {
@@ -59,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         int id = item.getItemId();
         if (id == R.id.action_ajustes){
-            viewSettings();// aqui habia algo
+            viewSettings();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -79,17 +75,12 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "onRequestPermissions");
         switch (requestCode){
             case CONTACTS_PERMISSION:
-                 if (grantResults[0] > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                      //permiso concedido
                      search();
-                 } else {
-                     //sin permiso
                  }
                  break;
         }
-        //requestCode
-        //permissions
-        //grantResults
     }
 
     @Override
@@ -114,27 +105,23 @@ public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void explain() {
         showRationaleDialog(getString(R.string.title)
-                            ,getString(R.string.message)
-                            ,Manifest.permission.READ_CONTACTS
-                            , CONTACTS_PERMISSION);
+                            ,getString(R.string.message));
         requestPermission();
     }
 
     //inicializamos los componentes
     private void initialize() {
-        btSearch = findViewById(R.id.btSearch);
+        Button btSearch = findViewById(R.id.btSearch);
         tvResult = findViewById(R.id.tvResult);
         etPhone = findViewById(R.id.etPhone);
+        SharedPreferences preferences = android.preference.PreferenceManager.getDefaultSharedPreferences(this);
+        String lastSearch = preferences.getString("last search", "");
 
-        btSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //String phoneNumber = String.valueOf(etPhone.getText());
-                searchIfPermitted();
+        if(!lastSearch.isEmpty()) {
+            etPhone.setText(lastSearch);
+        }
 
-
-            }
-        });
+        btSearch.setOnClickListener(view -> searchIfPermitted());
     }
 
     //PETICION DE PERMISO.
@@ -147,72 +134,46 @@ public class MainActivity extends AppCompatActivity {
     //ContentProvider Proveedor de contenidos
     //ContentResolver Consultor de contenidos
     private void search() {
-        // Queries the user dictionary and returns results
-        /*
-        Cursor  cursor = getContentResolver().query(
-                UserDictionary.Words.CONTENT_URI,                                               // The content URI of the words table
-                new String[] {"projection"},                                                    // The columns to return for each row (las columnas que queremos ver)
-                "campo 1 = ?and campo2 > ? and campo 3 = ?",                                     // Selection criteria (where) CONSULTA PREPARADA
-                new String[] {"pepe","4","23"},                                               // Selection criteria TANTOS COMO CAMPOS PREPARADOS HALLA
-                "campo5,campo3,campo4");                                                        // The sort order for the returned rows
+        tvResult.setText("");
+        String phone = etPhone.getText().toString();
+        phone = searchFormat(phone);
 
+        String cleanSearch = phone.replace("%", "");
+        SharedPreferences preferenciasActividad = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferenciasActividad.edit();
+        editor.putString("last search", cleanSearch);
+        editor.commit();
 
-        Uri uri = ContactsContract.Contacts.CONTENT_URI;
-        String proyeccion[] = new String[] {ContactsContract.Contacts.DISPLAY_NAME};
-        String seleccion = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = ? and " +
-                ContactsContract.Contacts.HAS_PHONE_NUMBER + "= ?";
-        String argumentos[] = new String[]{"1","1"};
-        seleccion = null;
-        argumentos = null;
-        String orden = ContactsContract.Contacts.DISPLAY_NAME + " collate localized asc";
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this );
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString("ved", "v");
+
+        tvResult.append("Last search: "+sharedPreferences.getString("last search", "")+"\n\n");
+
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] proyeccion = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+        String seleccion = ContactsContract.CommonDataKinds.Phone.NUMBER + " like ?";
+        String[] argumentos = new String[]{ phone };//etPhone.getText().toString()};
+        String orden = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
         Cursor cursor = getContentResolver().query(uri, proyeccion, seleccion, argumentos, orden);
         String[] columnas = cursor.getColumnNames();
-        for (String c : columnas){
-            Log.v(TAG, c);// saca las cosas por el logcat
-        }
-
-        String displayName;
-        int columna = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-        while (cursor.moveToNext()){
-            displayName = cursor.getString(columna);
-            Log.v(TAG, displayName);
-        }
-
-*/
-
-        tvResult.setText("");
-        Uri uri2 = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String proyeccion2[] = new String[] {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.Contacts.DISPLAY_NAME};
-        String seleccion2 =ContactsContract.CommonDataKinds.Phone.NUMBER + " like ?";                    // ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?";
-        String argumentos2[] = new String[]{etPhone.getText().toString()+"%"};                //new String[]{id+""};
-        String orden2 = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
-        Cursor cursor2 = getContentResolver().query(uri2, proyeccion2, seleccion2, argumentos2, orden2);
-        String[] columnas2 = cursor2.getColumnNames();
-
-        for (String c : columnas2){
-            Log.v(TAG, c);// saca las cosas por el logcat
-        }
-
-        int columnaNombre = cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        int columnaNumero = cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-        String nombre,numero;
-
-        while (cursor2.moveToNext()){
-            nombre = cursor2.getString(columnaNombre);
-            numero = cursor2.getString(columnaNumero);
-            //Log.v(TAG, c)
+        int columnaNombre = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        int columnaNumero = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+        String nombre, numero;
+        while(cursor.moveToNext()) {
+            nombre = cursor.getString(columnaNombre);
+            numero = cursor.getString(columnaNumero);
             tvResult.append("nombre: "+nombre+"\n numero: "+numero+"\n\n");
-    /*
-            for (String c : columnas2){
-                int pos = cursor2.getColumnIndex(c);
-                String valor = cursor2.getString(pos);
-                Log.v(TAG ,pos + " " + c +" " + valor);
-                tvResult.append(valor+"\n");
-            }
-
-     */
         }
+    }
 
+    private String searchFormat(String phone) {
+        String newString = "";
+        for (char ch: phone.toCharArray()) {
+            newString += ch + "%";
+        }
+        return newString;
     }
 
     private void searchIfPermitted() {
@@ -237,24 +198,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void showRationaleDialog(String title, String message, String permission, int requestCode){
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void showRationaleDialog(String title, String message){
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(title)
                 .setMessage(message)
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //no hago nada
-                    }
+                .setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                    //no hago nada
                 })
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        requestPermission();
-                    }
-                });
+                .setPositiveButton(R.string.ok, (dialogInterface, i) -> requestPermission());
         builder.create().show();
 
     }
